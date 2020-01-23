a62681e2b57d45e814e6fe6a8e2755786a48f922
Nick Falsitta <nickfalsitta@gmail.com>
2020-01-23 13:54:23 -0500

    added basic character slidey boy movement

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public Rigidbody rigidBod;
+    float speed = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +13,15 @@ public class CharacterMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetAxis("Forward") != 0)
+        {
+            rigidBod.AddForce(transform.forward * speed * Input.GetAxis("Forward"), ForceMode.Impulse);
+        }
+        if (Input.GetAxis("Sideways") != 0)
+        {
+            rigidBod.AddForce(transform.right * speed * Input.GetAxis("Sideways"), ForceMode.Impulse);
+        }
     }
 }
