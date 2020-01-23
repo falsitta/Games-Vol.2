05878574a68a4db8bc5a50cef11eadb12e9c45bf
Nick Falsitta <nickfalsitta@gmail.com>
2020-01-23 14:32:25 -0500

    changed gravity to -30, added  ajump but inconsistent jump heights

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,10 +6,20 @@ public class CharacterMovement : MonoBehaviour
 {
     public Rigidbody rigidBod;
     float speed = 2;
+    bool isJumping = false;
+
+    float distToGround;
+ 
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.001f);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        // get the distance to ground
+        distToGround = GetComponent<Collider>().bounds.extents.y;
     }
 
     // Update is called once per frame
@@ -17,11 +27,15 @@ public class CharacterMovement : MonoBehaviour
     {
         if (Input.GetAxis("Forward") != 0)
         {
-            rigidBod.AddForce(transform.forward * speed * Input.GetAxis("Forward"), ForceMode.Impulse);
+            rigidBod.AddForce(Vector3.forward * speed * Input.GetAxis("Forward"), ForceMode.Impulse);
         }
         if (Input.GetAxis("Sideways") != 0)
         {
-            rigidBod.AddForce(transform.right * speed * Input.GetAxis("Sideways"), ForceMode.Impulse);
+            rigidBod.AddForce(Vector3.right * speed * Input.GetAxis("Sideways"), ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.Space) && IsGrounded())
+        {
+            rigidBod.AddForce(Vector3.up * 60, ForceMode.Impulse);
         }
     }
 }
